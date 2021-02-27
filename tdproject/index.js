13c51da5ae92d086548698084c7bfb65833454da
Sagar mondal <Sagarm7@users.noreply.github.com>
2021-02-27 12:34:10 +0530

    new files
    new

--- tdproject/index.js
+++ tdproject/index.js
@@ -1,23 +1,21 @@
 let form = document.querySelector(".signup-form")
- let email = document.getElementById("email")
- let password = document.getElementById("password")
- let firstname = document.getElementById("first") 
- let surname = document.getElementById("sur")
- let locality = document.getElementById("local")
- let mobite = document.getElementById("phone")
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let emaillist = document.querySelector(".email-list")
+function afterSubmit(e){
+  // e.preventDefault()
+  if(email.value == "" || password.value == ""){
+    alert("Login credentials incorrect.")
+  }
+  else{
+    let item = document.createElement("li")
+    item.innerHTML = email.value
+    // console.log(item)
+    emaillist.appendChild(item)
+    email.value = ""
+    password.value = ""
 
+  }
 
- function afterSubmit(e){
-   e.preventDefault()
-   if(email.value == "" || password.value == ""){
-     alert("Login credentials incorrect.")
-   }
-//    else{
-//        let item = document.createElement("li")
-//        item.innerHTML = email.value
-//        emailList.appendChild(item)
-//        email.value = ""
-//        password.value = ""
-//    }
- }
- form.addEventListener("submit",afterSubmit)
+}
+form.addEventListener("submit", afterSubmit)
